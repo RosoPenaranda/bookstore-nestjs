19dc5ef015d9b218186c0045a6f052c2931d7b8f
Roso Peñaranda <rosopenaranda@gmail.com>
2020-06-20 17:41:44 -0500

    [REF] refactoring code

--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoleRepository } from './role.repository';
 import { RoleService } from './role.service';
-import { ShareModule } from 'src/shared/share.module';
 import { RoleController } from './role.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RoleRepository]), ShareModule],
+  imports: [TypeOrmModule.forFeature([RoleRepository])],
   providers: [RoleService],
   controllers: [RoleController],
 })
